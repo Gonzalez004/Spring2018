e69c85cbb4b17b9f8143bc399cead31ccff3e96e
Dark Stallion <gonzalez.ferny31@outlook.com>
2018-09-27 14:53:08 -0600

    Update CodeSource.cpp

--- PetSimFinal/CodeSource.cpp
+++ PetSimFinal/CodeSource.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <String>
+#include <string>
+#include <ctime>
 #include "Pet.h"
 using namespace std;
 
-char ChoicePet;
+string ChoicePet;
 string PetName;
 int PetType = 0;
 enum PetType { START, DOG, CAT, HEDGEHOG, HORSE };
@@ -11,33 +12,31 @@ enum PetType { START, DOG, CAT, HEDGEHOG, HORSE };
 
 int main() {
 	Dog Pet;
-
+	srand(time(NULL));
 
 	//cout << "Your pet " << Name << " Has been adpoted by you!" << endl;
 	//cout << "Coungraluation" << endl;
+	while (1) {
+		switch (PetType) {
+		case START:
+			cout << "Welcome to Pet Sim!" << endl;
+			cout << "What pet do you want?" << endl;
+			cout << "Dog(D)?" << endl;
+			getline(cin, ChoicePet);
 
-	switch (PetType) {
-	case START:
-		cout << "Welcome to Pet Sim!" << endl;
-		cout << "What pet do you want?" << endl;
-		cout << "Dog(D)?" << endl;
-		cin >> ChoicePet;
 
-		
-		if (ChoicePet == 'D') {
-			cout << "What is the name of your Dog?" << endl;
-			//getline(cin, PetName);
-			//cin >> PetName;
-			
-			Pet.InitDog(PetName);
-			PetType = DOG;
-			cout << "flag1";
-		}
-		break;
-	case DOG:
-		Pet.PrintInfo();
-		system("pause");
-		break;
-	}//Switch
+			if (ChoicePet.compare("Dog") == 0) {
+				cout << "What is the name of your Dog?" << endl;
+				getline(cin, PetName);
+				Pet.InitDog(PetName);
+				PetType = DOG;
+			}
+			break;
+		case DOG:
+			Pet.PrintInfo();
+			system("pause");
+			break;
+		}//Switch
+	}//while
 	return 0;
 }//Main
