f1840c7ed4a248657ff43687269d5adf4ac3517b
Dark Stallion <gonzalez.ferny31@outlook.com>
2018-10-01 14:50:02 -0600

    Update Pet.cpp

--- PetSimFinal/Pet.cpp
+++ PetSimFinal/Pet.cpp
@@ -1,28 +1,35 @@
 #include "Pet.h"
 /////////////////////////////////////////////////////////////////////////////////
 void Pet::Feed() {
+	HighScore =HighScore+1;
 	Hunger -= rand() % 20;
 }
 void  Pet::Play() {
+	HighScore = HighScore + 1;
 	Bored -= rand() % 20;
 }
 void  Pet::Clean() {
-	Cleanliness += rand() % 20;
+	HighScore = HighScore + 1;
+	Dirty -= rand() % 20;
 }
 void  Pet::Water() {
+	HighScore = HighScore + 1;
 	Thirst -= rand() % 20;
 }
 void  Pet::Bathroom() {
+	HighScore = HighScore + 1;
 	PeepPoop -= rand() % 20;
 }
 void  Pet::Medicine() {
+	HighScore = HighScore + 1;
 	Health += rand() % 20;
 }
 void  Pet::PrintInfo() {
 	cout << "Your pet " << Name << "'s stats:" << endl;
 	cout << "The hunger level is " << Hunger << endl;
-	cout << "The level of cleanliness is " << Cleanliness << endl;
-	cout << "The level of thirst is " << Thirst << endl;
+	cout << "The boredness level is " << Bored << endl;
+	cout << "The dirty level is " << Dirty << endl;
+	cout << "The thirst level is " << Thirst << endl;
 	cout << "The need to go to the bathroom is " << PeepPoop << endl;
 	cout << "The overall health of your pet is " << Health << endl;
 }
@@ -42,11 +49,11 @@ void Pet::Restrictions() {
 		Bored = 100;
 	}
 	////////////////////
-	if (Cleanliness < 0) {
-		Cleanliness = 0;
+	if (Dirty < 0) {
+		Dirty = 0;
 	}
-	if (Cleanliness > 100) {
-		Cleanliness = 100;
+	if (Dirty > 100) {
+		Dirty = 100;
 	}
 	////////////////////
 	if (Thirst < 0) {
@@ -70,110 +77,113 @@ void Pet::Restrictions() {
 		Health = 100;
 	}
 }
-void Pet::GoDown() {
-	int Decision;
-	////////////////////
+////////////////////
+void Pet::GoDownHunger() {
 	Decision = rand() % 2;
 	if (Decision == 0) {
 		Hunger += rand() % 10;
 	}
-	////////////////////
+}
+////////////////////
+void Pet::GoDownBored() {
 	Decision = rand() % 2;
 	if (Decision == 0) {
 		Bored += rand() % 10;
 	}
-	////////////////////
+}
+////////////////////
+void Pet::GoDownDirty() {
 	Decision = rand() % 2;
 	if (Decision == 0) {
-			Cleanliness -= rand() % 10;
+		Dirty += rand() % 10;
 	}
-	////////////////////
+}
+////////////////////
+void Pet::GoDownThirst() {
 	Decision = rand() % 2;
 	if (Decision == 0) {
 		Thirst += rand() % 10;
 	}
-	////////////////////
+}
+////////////////////
+void Pet::GoDownPeepPoop() {
 	Decision = rand() % 2;
 	if (Decision == 0) {
 		PeepPoop += rand() % 10;
 	}
 }
 void Pet::Die() {
-	/*	Hunger -= rand() % 20;
-	Bored -= rand() % 20;
-	Cleanliness += rand() % 20;
-	Thirst -= rand() % 20;
-	PeepPoop -= rand() % 20;
-	Health += rand() % 20;
-	*/
 	////////////////////
 	if (Hunger > 90) {
-		Health -= 10 ; 
+		Health -= 20;
 	}
-	else if (Hunger < 89 && Hunger > 50) {
+	else if (Hunger < 90 && Hunger > 50) {
+		Health -= 10;
+	}
+	else if (Hunger < 50 && Hunger > 25) {
 		Health -= 5;
 	}
 	////////////////////
 	if (Bored > 90) {
 		Health -= 5;
 	}
-	else if (Bored < 89 && Bored > 50) {
+	else if (Bored < 90 && Bored > 50) {
 		Health -= 2;
 	}
+	else if (Bored < 50 && Bored > 25) {
+		Health -= 1;
+	}
 	////////////////////
-	if (Cleanliness < 10) {
+	if (Dirty > 90) {
 		Health -= 10;
 	}
-	else if (Cleanliness < 50 && Cleanliness > 11) {
+	else if (Dirty < 80 && Dirty > 50) {
+		Health -= 5;
+	}
+	else if (Dirty < 50 && Dirty > 25) {
 		Health -= 2;
 	}
 	////////////////////
 	if (Thirst > 90) {
-		Health -= 10;
+		Health -= 20;
 	}
-	else if (Thirst < 89 && Thirst > 50) {
+	else if (Thirst < 90 && Thirst > 50) {
 		Health -= 5;
+	}
+	else if (Thirst < 50 && Thirst > 25) {
+		Health -= 2;
 	}
 	////////////////////
 	if (PeepPoop > 90) {
 		Health -= 5;
 	}
-	else if (Bored < 89 && Bored > 50) {
+	else if (PeepPoop < 90 && PeepPoop > 50) {
 		Health -= 2;
 	}
+	else if (PeepPoop < 50 && PeepPoop > 25) {
+		Health -= 1;
+	}
+	////////////////////
 	if (Health < 0 || Health == 0) {
 		cout << "Your Pet Health Reached Under 0" << endl;
+		cout << "Your highscore is " << HighScore << endl;
 		cout << "Game Over" << endl;
 		system("pause");
 		exit(0);
 	}
 	////////////////////
 }
+
+
+
 /////////////////////////////////////////////////////////////////////////////////
 void Dog::InitDog(string N) {
-	Hunger = rand() % 25;
-	Bored = rand() % 25;
-	Cleanliness = rand() % 50 + 50;
-	Thirst = rand() % 25;
-	PeepPoop = rand() % 25;
+	Hunger = rand() % 10;
+	Bored = rand() % 10;
+	Dirty = rand() % 10;
+	Thirst = rand() % 10;
+	PeepPoop = rand() % 10;
 	Health = 90;
 	Name = N;
 }
-
-void Dog::PrintInfo() {
-	cout << "Your pet " << Name << "'s stats:" << endl;
-	cout << "The hunger level is " << Hunger << endl;
-	cout << "The boredom level is " << Bored << endl;
-	cout << "The level of cleanliness is " << Cleanliness << endl;
-	cout << "The level of thirst is " << Thirst << endl;
-	cout << "The need to go to the bathroom is " << PeepPoop << endl;
-	cout << "The overall health of your pet is " << Health << endl;
-}
 /////////////////////////////////////////////////////////////////////////////////
-/*	Hunger -= rand() % 20;
-	Bored -= rand() % 20;
-	Cleanliness += rand() % 20;
-	Thirst -= rand() % 20;
-	PeepPoop -= rand() % 20;
-	Health += rand() % 20;
-*/
